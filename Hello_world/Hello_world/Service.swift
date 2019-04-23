f513b7a98bf3a055eecc799e8a2631b4acf5686d
Archana-Hirapara-BR <archana.hirapara@beelinesoftwares.com>
2019-04-23 19:24:30 +0530

    Finish

--- Hello_world/Hello_world/Service.swift
+++ Hello_world/Hello_world/Service.swift
@@ -10,11 +10,6 @@ import Foundation
 
 public class Service
 {
-//    $ ssh-keygen -t rsa -b 4096 -C "archana.hirapara@beelinesoftwares.com"
-
-//   git config --global user.name "Archana-Hirapara-BR"
-//    git config --global user.email archana.hirapara@beelinesoftwares.com
-//    ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQCh3+Ra4tATwY5s2r3gMmef1FYdOslH+Zk1tlb7/mbVYs5Wp69+my4sifkzOXAnKuXfkQikfUQgjgwcfemDWIWeFFtEY53bAjc1QQJ0VdFUvdl7TUQnQU0G54rrOCCB1QE0HPoI81jV+MYbuK5osNNBlgqbt171AQ2VgEGDLqUY04P0Qx60wTkhX2PMr3Yw0HBUU0Yo2vnksKYRIjww/AoiucIm9Nsgaa9QREMQpB/APZ6ECG7drno4/2m5q2o/HP+2vwQLhUqTzGVvxIp+l7oX7WQMlAJp6Aa7rVjcaRqtxYcyBu4qnrXvUvodPQJA0IrKWOfrK4dOP12kAf4EsKPzFJApvc5fswNab7jw2Zokyb55NJRJ0Nuk8lii8j9AYhI4M3e/D0Sa5/HC+fBSSLh3Z7pzPPSUoYaFgDxMNzrYkWWwdsuYO7we8gZcwrMGyBmUgpwhD7/Z8VdLLMJV55Q2gcAh+ZDYRmUJW7YwRTrcygVWc+mQXNNFakvhvUCcfzysSv7c9HzEmiREXZUic5+wdvvQ7wyAYqHSYkx6+6EzuaXse8hGAgZ6HqyYltvacSHRogdJ3uTcCUZ2MjYrXdmdSXWiG+r/gyhkm6HyHbnaHC+w2Gjc1OqgrzfiYU33lp1VtqESU4+ru1g5WQ5oHPREfsbFwFfl2XvtV9X8PHiIXQ== archana.hirapara@beelinesoftwares.com
 
     private init() {}
     
